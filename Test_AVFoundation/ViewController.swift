f8d259a5ba5dbd548398dc04f577627676e7c751
JaeWooYOO <yjw127588@snaptag.co.kr>
2023-07-06 15:03:17 +0900

    Initial Commit

---
+++ Test_AVFoundation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_AVFoundation
+//
+//  Created by 스냅태그 on 2023/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test_AVFoundationUITests/Test_AVFoundationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Test_AVFoundationUITestsLaunchTests.swift
+//  Test_AVFoundationUITests
+//
+//  Created by 스냅태그 on 2023/07/06.
+//
+
+import XCTest
+
+final class Test_AVFoundationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
